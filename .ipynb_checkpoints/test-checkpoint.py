35472190e6168e6570cc14a84918af92d1386dd5
bautistasch <bschneeberger@itba.edu.ar>
2024-10-16 14:33:36 -0300

    Remove unwanted ipynb file

--- .ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import os
-from attacker_detector.is_human_speaker import is_human_speaker
-
-path = 'audios/'
-audios_path = [os.path.join(path, file) for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
-
-for audio_path in audios_path:
-    print(audio_path, is_human_speaker(audio_path))
--- attacker_detector/.ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,3 +0,0 @@
-Python = 3.9.19
-
-`pip install -r requirements.txt`
